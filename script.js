0aedb4b4cb4973d9d8443fe1fb4ee74c65f2108c
Dyego27 <dyegoid123@gmail.com>
2025-03-01 18:30:48 -0300

    btt expandir feito

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ function selectLink(){
     )
     this.classList.add('ativo');
 }
+
 menuItem.forEach((item)=>
     
     item.addEventListener('click',selectLink)
 )
+
+var btnExp = document.querySelector('#btn-exp');
+var menuSide = document.querySelector('.menuLateral');
+
+btnExp.addEventListener('click',function(){
+    menuSide.classList.toggle('expandir');
+})
